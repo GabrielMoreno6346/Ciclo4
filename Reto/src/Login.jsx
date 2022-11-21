d8bcfc2267b6468db4fb9e6a916d160c2144e818
GabrielMoreno6346 <g.k.m.z@hotmail.com>
2022-11-21 08:17:08 -0500

    Working on Login

--- Reto/src/Login.jsx
+++ Reto/src/Login.jsx
@@ -2,16 +2,22 @@ import './static/Login.css'
 import {Button} from './components/Button'
 import {Input} from './components/Input'
 import {Link} from './components/Link'
+import {NavBar} from './components/NavBar'
+import {Footer} from './components/Footer'
 
 function Login() {
   return (
     <div className='LoginBody'>
-      <div className='LoginForm'>
-        <Link addr='/SignUp' LDisplay="Registrarse"></Link>
-        <Input holder="Usuario"></Input>
-        <Input holder="Contraseña"></Input>
-        <Button BDisplay="Login" addr="/OrdersList"></Button>
+      <Login></Login>
+      <div className='LoginContent'>
+        <div className='LoginForm'>
+          <Link addr='/SignUp' LDisplay="Registrarse"></Link>
+          <Input holder="Usuario"></Input>
+          <Input holder="Contraseña"></Input>
+          <Button BDisplay="Login" addr="/OrdersList"></Button>
+        </div>
       </div>
+      <Footer></Footer>
     </div>
   )
 }
